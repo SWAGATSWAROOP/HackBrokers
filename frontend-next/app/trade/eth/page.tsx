5441a80bde6d792b5d3b889555b0c22b5128a8f1
Satyam Gupta <guptasatyam1709@gmail.com>
2024-06-04 10:09:54 +0530

    merge conflict resolve

--- frontend-next/app/trade/eth/page.tsx
+++ frontend-next/app/trade/eth/page.tsx
@@ -3,27 +3,16 @@ import { useState, useEffect } from 'react';
 import axios from 'axios';
 import Image from 'next/image';
 
-<<<<<<< HEAD
 export default function ETHCARD() {
-=======
-export default function BitcoinCard() {
->>>>>>> 579d62cc456506953d77ae8e06316ded92a22f4e
     const [days, setDays] = useState(7);
     const [imageUrl, setImageUrl] = useState('https://res.cloudinary.com/djtudleky/image/upload/v1717468043/ogib3zfczrxpyfeo6mod.png');
-
     useEffect(() => {
         const fetchImage = async () => {
-<<<<<<< HEAD
             console.log("Change occured");
             try {
                 const res = await axios.get(`http://127.0.0.1:5000/upload?days=${days}&type=eth`);
                 console.log(res);
                 setImageUrl(res.data.secure_url);
-=======
-            try {
-                const res = await axios.get(`/upload?days=${days}&type=btc`);
-                setImageUrl(res.secure_url.imageUrl);
->>>>>>> 579d62cc456506953d77ae8e06316ded92a22f4e
             } catch (error) {
                 console.error('Error fetching image:', error);
             }
@@ -53,16 +42,9 @@ export default function BitcoinCard() {
                         className="rounded"
                     />
                     <div className="px-6 py-4">
-<<<<<<< HEAD
-                        <div className="font-bold text-xl mb-2">Bitcoin (BTC)</div>
-                        <p className="text-gray-700 text-base">
-                            Bitcoin is a decentralized digital currency, without a central bank or single administrator, that can be sent from user to user on the peer-to-peer bitcoin network without the need for intermediaries.
-=======
                         <div className="font-bold text-xl mb-2"> Etherium (ETH)</div>
                         <p className="text-gray-700 text-base">
                         Ethereum is a decentralized blockchain with smart contract functionality. Ether is the native cryptocurrency of the platform. Among cryptocurrencies, ether is second only to bitcoin in market capitalization. It is open-source software. Ethereum was conceived in 2013 by programmer Vitalik Buterin. 
-
->>>>>>> 579d62cc456506953d77ae8e06316ded92a22f4e
                         </p>
                     </div>
                 </div>
