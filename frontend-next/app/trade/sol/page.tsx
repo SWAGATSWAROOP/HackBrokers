358fd0c07508f74e14d76c0f7d90f5a4038b1c8b
Satyam Gupta <guptasatyam1709@gmail.com>
2024-06-04 09:46:02 +0530

    url set for image

--- frontend-next/app/trade/sol/page.tsx
+++ frontend-next/app/trade/sol/page.tsx
@@ -1,16 +1,55 @@
+'use client'
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import Image from 'next/image';
 
-import { useRouter } from "next/navigation";
-import Link from "next/link";
-import AuthButtons from "./forms/authbutton";
-import Nav from "../Components/Nav";
-import { useState } from "react";
-export default function Trade() {
+export default function SOLCard() {
+    const [days, setDays] = useState(7);
+    const [imageUrl, setImageUrl] = useState('https://res.cloudinary.com/djtudleky/image/upload/v1717474460/wt1gtzmvctpw7ehtilpk.png');
+
+    useEffect(() => {
+        const fetchImage = async () => {
+            console.log("Change occured");
+            try {
+                const res = await axios.get(`http://127.0.0.1:5000/upload?days=${days}&type=sol`);
+                console.log(res);
+                setImageUrl(res.data.secure_url);
+            } catch (error) {
+                console.error('Error fetching image:', error);
+            }
+        };
+        
+        fetchImage();
+    }, [days]);
+
     return (
-        <>
-            <div className="text-xl text-white font-serif  ">
-                <h1 className="m-20"> (SOL):</h1>
-                {/* api fetch */}
+        <div className="flex justify-center items-center min-h-screen bg-gray-900">
+            <div className="max-w-sm rounded overflow-hidden shadow-lg bg-white m-4">
+                <div className="flex flex-row items-center p-4">
+                    <select
+                        className="mb-4 p-2 border rounded"
+                        value={days}
+                        onChange={(e:any) => setDays(e.target.value)}
+                    >
+                        <option value={7}>7 Days</option>
+                        <option value={30}>30 Days</option>
+                        <option value={365}>365 Days</option>
+                    </select>
+                    <img
+                        src={imageUrl}
+                        alt="Bitcoin Image"
+                        width={300}
+                        height={300}
+                        className="rounded"
+                    />
+                    <div className="px-6 py-4">
+                        <div className="font-bold text-xl mb-2">Bitcoin (BTC)</div>
+                        <p className="text-gray-700 text-base">
+                            Bitcoin is a decentralized digital currency, without a central bank or single administrator, that can be sent from user to user on the peer-to-peer bitcoin network without the need for intermediaries.
+                        </p>
+                    </div>
+                </div>
             </div>
-        </>
+        </div>
     );
 }
