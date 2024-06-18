333e0d7e23b888acb468bc42fff31524327d54a5
SWAGAT SWAROOP PARIDA <swagatswaroop@gmail.com>
2024-06-19 02:55:19 +0530

    Chnaged Unused variable

--- frontend-next/app/market/TradeNavbar.tsx
+++ frontend-next/app/market/TradeNavbar.tsx
@@ -1,12 +1,8 @@
 import Link from "next/link";
-import { Next_Auth_Config } from "../lib/auth";
-import { getServerSession } from "next-auth";
-import { buttonVariants } from "../Components/ui/button";
 
 export default async function TradeNavbar() {
-  const session = await getServerSession(Next_Auth_Config);
   return (
-    <div className="fixed left-0 right-0 top-0 z-10 border-b bg-gradient-to-r from-indigo-900 via-purple-900 to-pink-500 from-30% py-2">
+    <div className="fixed left-0 right-0 top-0 z-10 border-b bg-gradient-to-r from-indigo-900 from-30% via-purple-900 to-pink-500 py-2">
       <div className="container mx-auto flex items-center justify-between">
         <Link
           href={"/"}
@@ -15,14 +11,11 @@ export default async function TradeNavbar() {
           🅑🅔 CRYPTOxAI
         </Link>
         <Link
-            href={"/deposit"}
-            className="bg-black hover:bg-gradient-to-r from-black from-10% to-blue-500 px-4 py-2 text-white rounded-xl"
-          >
+          href={"/deposit"}
+          className="rounded-xl bg-black from-black from-10% to-blue-500 px-4 py-2 text-white hover:bg-gradient-to-r"
+        >
           <h1 className="font-serif">Deposit Money</h1>
-          </Link>
-      
-          
-   
+        </Link>
       </div>
     </div>
   );
